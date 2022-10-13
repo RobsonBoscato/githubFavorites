a106806d0c4f1fce422083df61f764de46380f30
RobsonBoscato <rbsn.boscato@gmail.com>
2022-10-13 11:45:05 -0300

    created API module

--- js/favorites.js
+++ js/favorites.js
@@ -1,45 +1,49 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`;
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(data => ({
-        login: data.login,
-        name: data.name,
-        public_repos: data.public_repos,
-        followers: data.followers,
-      }));
-  }
-}
-
+import { GithubUser } from './githubUser.js';
 //classe que vai conter a logica dos dados e estruturação
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
-
-    GithubUser.search('uzumaki').then(user => console.log(user));
   }
 
   load() {
-    const entries = JSON.parse(localStorage.getItem('@github-favorites')) || [];
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
+  }
 
-    this.entries = [];
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries));
   }
 
   async add(username) {
-    const user = await GithubUser.search(username);
+    try {
+      const userExists = this.entries.find(entry => entry.login === username);
+
+      if (userExists) {
+        throw new Error('Usuário já cadastrado');
+      }
+
+      const user = await GithubUser.search(username);
+
+      if (user.login === undefined) {
+        throw new Error('Usuário não encontrado!');
+      }
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   delete(user) {
     const filteredEntries = this.entries.filter(
-      entry => entry.user_Login !== user.user_Login
+      entry => entry.login !== user.login
     );
 
     this.entries = filteredEntries;
-
     this.update();
+    this.save();
   }
 }
 
@@ -96,27 +100,25 @@ export class FavoritesView extends Favorites {
 
       row.querySelector(
         '.user img'
-      ).src = `https://github.com/${user.user_Login}.png`;
+      ).src = `https://github.com/${user.login}.png`;
 
-      row.querySelector('.user img').alt = `imagem de ${user.user_Login}`;
+      row.querySelector('.user img').alt = `imagem de ${user.login}`;
 
       row.querySelector(
         '.user a'
-      ).href = `https://github.com/${user.user_Login}`;
+      ).href = `https://github.com/users/${user.login}`;
 
-      row.querySelector('.user a p').textContent = user.user_Name;
+      row.querySelector('.user a p').textContent = user.name;
 
-      row.querySelector('.user a span').textContent = user.user_Login;
+      row.querySelector('.user a span').textContent = user.login;
 
-      row.querySelector('.repositories').textContent = user.user_Repos;
+      row.querySelector('.repositories').textContent = user.public_repos;
 
-      row.querySelector('.followers').innerHTML = user.user_Followers;
+      row.querySelector('.followers').innerHTML = user.followers;
       this.tbody.append(row);
 
       row.querySelector('.remove').onclick = () => {
-        const confirmed = confirm(
-          'Tem certeza que deseja remover esse usuário da lista?'
-        );
+        const confirmed = confirm('Deseja remover esse usuário da lista?');
         if (confirmed) {
           this.delete(user);
         }
